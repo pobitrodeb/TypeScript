00eba7202dec72a5a27e284ef5f62b517736ec4b
Pobitrodeb <pobitrodn@gmail.com>
2023-04-24 12:27:46 +0600

    param type set added

--- output/script.js
+++ output/script.js
@@ -1,3 +1,6 @@
 "use strict";
-const country = "I love Bangladesh";
-console.log(country);
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply('pobitrodeb', 15)); // This is not working because when I send param I decleared param type() a: number)
+console.log(multiply(3, 4));
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,6 @@
-const country = "Bangladesh";
+function multiply(a : number, b : number){
+    return a*b; 
+}
 
-console.log(country);
+console.log(multiply('pobitrodeb', 15)); // This is not working because when I send param I decleared param type() a: number)
+console.log(multiply(3,4));
