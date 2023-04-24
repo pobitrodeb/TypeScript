e2c4971e7d9e21761e43a18706eabac029d3d48c
Pobitrodeb <pobitrodn@gmail.com>
2023-04-24 12:38:07 +0600

    arry data type checked

--- src/script.ts
+++ src/script.ts
@@ -4,3 +4,15 @@ function multiply(a : number, b : number){
 
 console.log(multiply('pobitrodeb', 15)); // This is not working because when I send param I decleared param type() a: number)
 console.log(multiply(3,4));
+
+
+// Array Type 
+let friuts = ['Apple', 'Banana', 'Orange']; 
+let newFriuts = friuts.push ('mango'); 
+console.log(newFriuts);
+
+// When you send mix array 
+
+let mixData = ['12', 'Fist name ', true, 'Ok']; 
+let newMixData = mixData.push('New Data'); 
+console.log(newMixData);
